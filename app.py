611474f72ca487ca3a037ab3c1596c7346692a8e
HendrikVE <hendrik1995@msn.com>
2017-11-09 20:34:01 +0100

    fixed extedning single int

--- app.py
+++ app.py
@@ -113,8 +113,8 @@ def get_user_input(after_input_func, prompt=""):
     allowed_keycodes.extend(range(ord("0"), ord("9")+1))   # numbers
     allowed_keycodes.extend(range(ord("A"), ord("Z")))     # uppercase letters
     allowed_keycodes.extend(range(ord("a"), ord("z")))     # lowercase letters
-    #allowed_keycodes.extend(8)                             # backspace
-    allowed_keycodes.extend(13)                            # enter
+    #allowed_keycodes.append(8)                             # backspace
+    allowed_keycodes.append(13)                            # enter
 
     sys.stdout.write(prompt)
 
