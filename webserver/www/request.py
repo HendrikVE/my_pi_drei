52281bcdaee086199d09418f5e9b9762a8a05bb6
HendrikVE <hendrik1995@msn.com>
2017-11-10 17:19:30 +0100

    new file structure and check for signature before running code

--- webserver/www/request.py
+++ webserver/www/request.py
@@ -4,21 +4,98 @@
 from __future__ import print_function
 
 import cgi
+import hashlib
+import hmac
+import json
+import logging
+
+import sys
+
+import os
+import tempfile
+
+import time
+import uuid
+
 from Display import Display
 
-form = cgi.FieldStorage()
+LOGFILE = 'request.log'
+LOGGING_FORMAT = '[%(levelname)s]: %(asctime)s\n'\
+                 + 'in %(filename)s in %(funcName)s on line %(lineno)d\n'\
+                 + '%(message)s\n'
 
-action = form.getfirst('action')
+SECRET_KEY = 'meinkey'
 
-print ('Content-Type: text/html')
-print ('\n\r')
+pi_results = {
+    'displayState': None, # on or off
+    'displayIntensity': None, # 0-1023
+    'temperature': None, # int
+    'humidity': None, # int
+    'error': None,
+}
 
-print('action = ' + action)
 
-display = Display()
-if action == "1":
-    display.turn_on()
-    print('display turned on')
-elif action == "2":
-    display.turn_off()
-    print('display turned off')
+def main():
+
+    request_body = sys.stdin.read()
+    pi_results['temperature'] = request_body
+    is_valid = is_valid_signature(os.environ['HTTP_MESSAGE_SIGNATURE'], SECRET_KEY, request_body)
+
+    if is_valid:
+
+        # stdin already read out, so assign fp to FieldStorage
+        tmpfile = tempfile.TemporaryFile()
+        tmpfile.write(request_body)
+        tmpfile.seek(0)
+        form = cgi.FieldStorage(fp=tmpfile)
+        tmpfile.close()
+
+        action = form.getfirst('action')
+
+        display = Display()
+        if action == '1':
+            display.turn_on()
+            pi_results['displayState'] = 'on'
+        elif action == '2':
+            display.turn_off()
+            pi_results['displayState'] = 'off'
+
+        print_result(json.dumps(pi_results))
+
+    else:
+        print_error()
+
+
+def is_valid_signature(signature, secret_key, body):
+
+    computed_signature = 'sha512=' + hmac.new(secret_key, body, hashlib.sha512).hexdigest()
+
+    return computed_signature == signature
+
+
+def print_result(result):
+
+    print ('Content-Type: text/html')
+    print ('\n\r')
+    print (result)
+
+
+def print_error():
+
+    print ('Status: 403 Forbidden')
+    print ('\n\r')
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(filename=LOGFILE, format=LOGGING_FORMAT,
+                        datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+
+    try:
+        main()
+
+    except Exception as e:
+        logging.error(str(e), exc_info=True)
+        pi_results['error'] = str(e)
+
+        print_result(json.dumps(pi_results))
