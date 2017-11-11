243f0be1bbcec8d2622ff17156fd632d1e1a8f13
HendrikVE <hendrik1995@msn.com>
2017-11-11 02:13:07 +0100

    apache setup

--- setup.py
+++ setup.py
@@ -12,4 +12,36 @@ sudo python setup.py install
 
 DISPLAY
 
+
+APACHE
+
+sudo a2enmod userdir
+
+/etc/apache2/sites-available/mypidrei.com.conf
+####################################
+<VirtualHost 127.0.0.2:80>
+
+        <Directory /var/www/my_pi_drei/www/>
+                Options +ExecCGI
+                DirectoryIndex index.py
+        </Directory>
+        AddHandler cgi-script .py
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/my_pi_drei/www/
+        ServerName www.mypidrei.com
+        ServerAlias mypidrei.com
+
+</VirtualHost>
+####################################
+sudo a2ensite mypidrei.com.conf
+sudo service apache2 restart
+
+
+add in /etc/hosts
+####################################
+127.0.0.2       mypidrei.com
+####################################
+
+
 """
