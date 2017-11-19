464ceaf20a89bce45bbd8b2c2ca5b4c0e1ebe5cf
HendrikVE <hendrik1995@msn.com>
2017-11-19 21:29:03 +0100

    new way of coloring output

--- client_app/util/colored_print.py
+++ client_app/util/colored_print.py
@@ -5,40 +5,47 @@ from __future__ import print_function
 
 import sys
 
-_RED = "\033[1;31m"
-_BLUE = "\033[1;34m"
-_CYAN = "\033[1;36m"
-_GREEN = "\033[0;32m"
-_RESET = "\033[0;0m"
-_BOLD = "\033[;1m"
-_REVERSE = "\033[;7m"
+RED = '\033[1;31m'
+BLUE = '\033[1;34m'
+CYAN = '\033[1;36m'
+GREEN = '\033[0;32m'
+YELLOW = '\[\033[1;33m\]'
+
+BOLD = '\033[;1m'
+REVERSE = '\033[;7m'
+
+RESET = '\033[0;0m'
+
+
+def styled_text(text, color):
+    return color + text + RESET
 
 
 def print_red(output_string):
-    _colored_print(output_string, _RED)
+    _colored_print(output_string, RED)
 
 
 def print_blue(output_string):
-    _colored_print(output_string, _BLUE)
+    _colored_print(output_string, BLUE)
 
 
 def print_cyan(output_string):
-    _colored_print(output_string, _CYAN)
+    _colored_print(output_string, CYAN)
 
 
 def print_green(output_string):
-    _colored_print(output_string, _GREEN)
+    _colored_print(output_string, GREEN)
 
 
 def print_bold(output_string):
-    _colored_print(output_string, _BOLD)
+    _colored_print(output_string, BOLD)
 
 
 def print_reverse(output_string):
-    _colored_print(output_string, _REVERSE)
+    _colored_print(output_string, REVERSE)
 
 
 def _colored_print(output_string, style):
     sys.stdout.write(style)
     sys.stdout.write(output_string)
-    print(_RESET)
+    print(RESET)
