3d95363a401002fbbc4bae241d20146f055df311
HendrikVE <hendrik1995@msn.com>
2017-11-19 21:36:57 +0100

    added colors

--- client_app/util/colored_print.py
+++ client_app/util/colored_print.py
@@ -5,11 +5,23 @@ from __future__ import print_function
 
 import sys
 
-RED = '\033[1;31m'
-BLUE = '\033[1;34m'
-CYAN = '\033[1;36m'
+BLACK = '\033[0;30m'
+GREY_DARK = '\033[1;30m'
+GREY_LIGHT = '\033[0;37m'
+RED = '\033[0;31m'
+RED_LIGHT = '\033[1;31m'
 GREEN = '\033[0;32m'
-YELLOW = '\[\033[1;33m\]'
+GREEN_LIGHT = '\033[1;32m'
+BROWN = '\033[0;33m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[0;34m'
+BLUE_LIGHT = '\033[1;34m'
+PURPLE = '\033[0;35m'
+PURPLE_LIGHT = '\033[1;35m'
+CYAN = '\033[0;36m'
+CYAN_LIGHT = '\033[1;36m'
+WHITE = '\033[1;37m'
+COLORLESS = '\033[0m'
 
 BOLD = '\033[;1m'
 REVERSE = '\033[;7m'
