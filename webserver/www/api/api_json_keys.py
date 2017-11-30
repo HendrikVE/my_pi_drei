aaa233dc69d29570e5133074f8c1f38997c02cce
HendrikVE <hendrik1995@msn.com>
2017-11-30 12:00:33 +0100

    removed unused keys

--- webserver/www/api/api_json_keys.py
+++ webserver/www/api/api_json_keys.py
@@ -5,21 +5,9 @@
 REQUEST_KEY_ACTION_KEY = 'action_key'
 REQUEST_KEY_ACTION_ARGUMENT = 'action_argument'
 
-REQUEST_KEY_GET_DISPLAY_STATE = 'get_display_state'
-REQUEST_KEY_SET_DISPLAY_STATE = 'set_display_state'
-REQUEST_KEY_GET_DISPLAY_INTENSITY = 'get_display_intensity'
-REQUEST_KEY_SET_DISPLAY_INTENSITY = 'set_display_intensity'
 REQUEST_KEY_GET_TEMPERATURE = 'get_temperature'
 REQUEST_KEY_GET_HEAT_INDEX = 'get_heat_index'
 REQUEST_KEY_GET_HUMIDITY = 'get_humidity'
-
-# RESULT KEYS
-RESULT_KEY_GET_DISPLAY_STATE = 'display_state'  # on or off
-RESULT_KEY_SET_DISPLAY_STATE = 'display_state'  # on or off
-RESULT_KEY_GET_DISPLAY_INTENSITY = 'display_intensity'  # 0-1023
-RESULT_KEY_SET_DISPLAY_INTENSITY = 'display_intensity'  # 0-1023
-RESULT_KEY_GET_TEMPERATURE = 'temperature'  # int
-RESULT_KEY_GET_HUMIDITY = 'humidity'  # int
 
 RESULT_KEY_API_VERSION = 'api_version'  # string
 RESULT_KEY_RESULT = 'result'  # string
