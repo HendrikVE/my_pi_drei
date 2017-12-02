4ab3f11d9b7a606aee0a327836990a7b4b4b8645
HendrikVE <hendrik1995@msn.com>
2017-12-02 18:14:21 +0100

    added doc to _ArduinoNano.py

--- hardware/arduino_nano/_ArduinoNano.py
+++ hardware/arduino_nano/_ArduinoNano.py
@@ -8,16 +8,23 @@
  * See the file LICENSE in the top level directory for more details.
 """
 
-import logging
-import serial
+from serial import Serial, SerialException
 import time
 
 
 class TempScale:
+    """
+    Possible scale types for requesting temperatures
+
+    """
     CELSIUS, FAHRENHEIT = range(2)
 
 
 class RequestData:
+    """
+    Serial requests for the device (dependant on code of the arduino)
+
+    """
     TEMP_CEL = 'temp_cel'
     TEMP_FAH = 'temp_fah'
     HEAT_INDEX_CEL = 'heat_index_cel'
@@ -34,9 +41,34 @@ class DeviceUnconnectedException(Exception):
 
 
 class ArduinoNano(object):
+    """
+    Abstraction for a connected Arduino Nano
+
+    Methods
+    -------
+    start_communication()
+        Initiate the communication with the Arduino Nano
+
+    stop_communication()
+        Stop the communication with the Arduino Nano
+
+    get_temperature(scale=TempScale.CELSIUS)
+        Read out temperature from Arduino Nano
+
+    get_heat_index(scale=TempScale.CELSIUS)
+        Read out heat index from Arduino Nano
+
+    get_humidity()
+        Read out humidity from Arduino Nano
+
+    """
+    _serialConnection = None
 
     class _SerialConnection(object):
+        """
+        Handling serial connection on USB
 
+        """
         usb_path = ''
         serial_connection = None
 
@@ -47,7 +79,7 @@ class ArduinoNano(object):
             self.close()
 
         def open(self):
-            self.serial_connection = serial.Serial(self.usb_path)
+            self.serial_connection = Serial(self.usb_path)
             time.sleep(5)  # wait for the arduino to reset after serial connection
 
         def close(self):
@@ -67,46 +99,111 @@ class ArduinoNano(object):
             except Exception:
                 raise DeviceUnconnectedException()
 
-    serialConnection = None
-
     def start_communication(self):
-        try:
-            self.serialConnection = self._SerialConnection('/dev/ttyUSB0')
-            self.serialConnection.open()
+        """
+        Establish the serial connection to the device
 
-        except Exception as e:
+        Raises
+        ------
+        SerialException
+            Raised if something went wrong opening a serial connection to the device
+
+        """
+        try:
+            self._serialConnection = self._SerialConnection('/dev/ttyUSB0')
+            self._serialConnection.open()
+
+        except SerialException as e:
             raise e
 
     def stop_communication(self):
-        self.serialConnection.close()
+        """
+        Close the serial connection to the device
+        """
+        self._serialConnection.close()
 
-    def get_temperature(self, scale):
+    def get_temperature(self, scale=TempScale.CELSIUS):
+        """
+        Read temperature from device
 
+        Parameters
+        ----------
+        scale : TempScale, optional
+            Used scale type for temperature. Default: TempScale.CELSIUS
+
+        Returns
+        -------
+        float
+            Temperature
+
+        Raises
+        ------
+        TypeError
+            In case of invalid scale type
+
+        DeviceUnconnectedException
+            Value could not be read, because device is not connected
+
+        """
         if scale == TempScale.CELSIUS:
-            result = self.serialConnection.request(RequestData.TEMP_CEL)
+            result = self._serialConnection.request(RequestData.TEMP_CEL)
             return float(result)
 
         elif scale == TempScale.FAHRENHEIT:
-            result = self.serialConnection.request(RequestData.TEMP_FAH)
+            result = self._serialConnection.request(RequestData.TEMP_FAH)
             return float(result)
 
         else:
             raise TypeError('not a valid TempScale')
 
-    def get_heat_index(self, scale):
+    def get_heat_index(self, scale=TempScale.CELSIUS):
+        """
+        Read heat index from device
 
+        Parameters
+        ----------
+        scale : TempScale, optional
+            Used scale type for temperature. Default: TempScale.CELSIUS
+
+        Returns
+        -------
+        float
+            Heat index
+
+        Raises
+        ------
+        TypeError
+            In case of invalid scale type
+
+        DeviceUnconnectedException
+            Value could not be read, because device is not connected
+
+        """
         if scale == TempScale.CELSIUS:
-            result = self.serialConnection.request(RequestData.HEAT_INDEX_CEL)
+            result = self._serialConnection.request(RequestData.HEAT_INDEX_CEL)
             return float(result)
 
         elif scale == TempScale.FAHRENHEIT:
-            result = self.serialConnection.request(RequestData.HEAT_INDEX_FAH)
+            result = self._serialConnection.request(RequestData.HEAT_INDEX_FAH)
             return float(result)
 
         else:
             raise TypeError('not a valid TempScale')
 
     def get_humidity(self):
+        """
+        Read humidity from device
 
-        result = self.serialConnection.request(RequestData.HUMIDITY)
+        Returns
+        -------
+        float
+            Humidity (percentage)
+
+        Raises
+        ------
+        DeviceUnconnectedException
+            Value could not be read, because device is not connected
+
+        """
+        result = self._serialConnection.request(RequestData.HUMIDITY)
         return float(result)
